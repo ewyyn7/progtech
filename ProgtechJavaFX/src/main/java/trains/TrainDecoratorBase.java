a3faf0cebb340aa01b568a1e9e23e0f05847f321
Rolidroid0 <budairoland0@gmail.com>
2024-05-06 20:21:46 +0200

    TrainDecoratorBase létrehozása

---
+++ ProgtechJavaFX/src/main/java/trains/TrainDecoratorBase.java
@@ -0,0 +1,29 @@
+package trains;
+
+public abstract class TrainDecoratorBase extends TrainBase {
+    protected TrainBase train;
+
+    public TrainDecoratorBase(TrainBase train){
+        this.train = train;
+    }
+
+    @Override
+    public String getModel() {
+        return train.getModel();
+    }
+
+    @Override
+    public int GetAverageSpeed() {
+        return train.GetAverageSpeed();
+    }
+
+    @Override
+    public int GetSafetyLevel() {
+        return train.GetSafetyLevel();
+    }
+
+    @Override
+    public int GetNumberOfWagons() {
+        return train.GetNumberOfWagons();
+    }
+}
