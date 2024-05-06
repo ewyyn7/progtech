8ba2b520222e4c6d137f0386a229e885127d3f0f
Rolidroid0 <budairoland0@gmail.com>
2024-05-06 19:12:06 +0200

    Vonat abstract base létrehozása

---
+++ ProgtechJavaFX/src/main/java/trains/TrainBase.java
@@ -0,0 +1,10 @@
+package trains;
+
+public abstract class TrainBase {
+    public abstract int getId();
+    public abstract int GetAverageSpeed();
+    public abstract int GetSafetyLevel();
+    public abstract int GetNumberOfWagons();
+    public abstract void saveToDatabase();
+    public abstract void loadFromDatabase(int id);
+}
