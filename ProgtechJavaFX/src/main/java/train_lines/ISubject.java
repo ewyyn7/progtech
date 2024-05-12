a41c2a4c785458b0b3d2d3061b1767eb94f62cb4
Törteli Márton <60424745+ewyyn7@users.noreply.github.com>
2024-05-12 22:08:01 +0200

    Train_line ISubject

---
+++ ProgtechJavaFX/src/main/java/train_lines/ISubject.java
@@ -0,0 +1,10 @@
+package train_lines;
+
+public interface ISubject {
+    void RegisterObserver(IObserver observer);
+
+    void RemoveObserver(IObserver observer);
+
+    void NotifyObservers();
+
+}
