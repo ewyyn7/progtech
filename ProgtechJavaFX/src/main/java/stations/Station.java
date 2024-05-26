96d7c7637e555c4b9d540be1311d5f13b3619178
Törteli Márton <60424745+ewyyn7@users.noreply.github.com>
2024-05-26 12:55:37 +0200

    Station osztály

---
+++ ProgtechJavaFX/src/main/java/stations/Station.java
@@ -0,0 +1,43 @@
+package stations;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Station {
+
+    public IntegerProperty idProperty() {
+        return new SimpleIntegerProperty(getId());
+    }
+    public StringProperty nameProperty() {
+        return new SimpleStringProperty(getName());
+    }
+
+    private int id;
+    private String name;
+
+    // Constructors, getters, and setters
+    public Station(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
