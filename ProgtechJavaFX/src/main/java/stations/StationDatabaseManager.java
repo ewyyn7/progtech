11543ac390da2d7653678e847bf6b5ce89776f56
Törteli Márton <60424745+ewyyn7@users.noreply.github.com>
2024-05-26 12:55:56 +0200

    Station db kezelés

---
+++ ProgtechJavaFX/src/main/java/stations/StationDatabaseManager.java
@@ -0,0 +1,98 @@
+package stations;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import databaseConnection.ConnectToDB;
+
+public class StationDatabaseManager {
+
+
+    public static void addStation(Station station) {
+
+
+        try { Connection conn = ConnectToDB.connect();
+            String sql = "INSERT INTO stations (id, name) VALUES (?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, station.getId());
+            stmt.setString(2, station.getName());
+            stmt.executeUpdate();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Station getStation(int id) {
+
+        Station station = null;
+
+        try {Connection conn = ConnectToDB.connect();
+            String sql = "SELECT * FROM stations WHERE id = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                String name = rs.getString("name");
+                station = new Station(id, name);
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return station;
+    }
+
+    public static List<Station> getAllStations() {
+
+        List<Station> stations = new ArrayList<>();
+
+
+        try {
+            Connection conn = ConnectToDB.connect();
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM stations";
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                stations.add(new Station(id, name));
+            }
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return stations;
+    }
+
+    public static void updateStation(Station station) {
+        String sql = "UPDATE stations SET name = ? WHERE id = ?";
+
+        try {Connection conn = ConnectToDB.connect();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, station.getName());
+            stmt.setInt(2, station.getId());
+            stmt.executeUpdate();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteStation(int id) {
+        String sql = "DELETE FROM stations WHERE id = ?";
+
+        try {Connection conn = ConnectToDB.connect();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
